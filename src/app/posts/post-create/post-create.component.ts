299cdc1b219559f214887f3482ee907ced91ac31
southheat92 <southheat92@gmail.com>
2018-06-17 13:15:29 -0400

    lecture 18. first steps with the angular/material package. sprucing up the simple app using some MaterialModules. worth rewatching and skimming this code

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-post-create', // selectors let you include the component elsewhere
-    templateUrl: './post-create.component.html' // could also do "template: [html]"
+    templateUrl: './post-create.component.html', // could also do "template: [html]
+    styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
     enteredValue = '';
