efdd1ab3842dfe7b8c9abc256fd5693f8ddd72da
southheat92 <southheat92@gmail.com>
2018-07-02 17:09:20 -0400

    lectures 23, 24. adding a post.model and also refactoring post-create.component.ts to use <form>s instead of the alternative. some relevant info in the notes for this. useful to go back over with the code.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,8 @@
 import {Component, EventEmitter, Output} from '@angular/core';
 
+import {Post } from '../post.model';
+import { NgForm } from '@angular/forms';
+
 @Component({
     selector: 'app-post-create', // selectors let you include the component elsewhere
     templateUrl: './post-create.component.html', // could also do "template: [html]
@@ -8,12 +11,15 @@ import {Component, EventEmitter, Output} from '@angular/core';
 export class PostCreateComponent {
     enteredContent = '';
     enteredTitle = '';
-    @Output() postCreated = new EventEmitter();
+    @Output() postCreated = new EventEmitter<Post>();
 
-    onAddPost() {
-      const post = {
-        title: this.enteredTitle,
-        content: this.enteredContent
+    onAddPost(form: NgForm) {
+      if (form.invalid) {
+        return;
+      }
+      const post: Post = {
+        title: form.value.title,
+        content: form.value.content
       };
 
       this.postCreated.emit(post);
