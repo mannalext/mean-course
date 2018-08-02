447749af10f860c1dff03b1a7986c9ad9f20ff85
Alex Mann <atmann@Alexs-MacBook-Pro-2.local>
2018-08-02 10:24:04 -0400

    lecture 36. fleshing out a route to get posts. using contrived examples of posts for now. just illustrating express middleware and crafting an http response

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('first middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('hello from express');
+app.use('/api/posts',(req, res, next) => {
+  const posts = [
+    {
+      id: 'fasfasdfavnlk',
+      title: 'First server side post',
+      content: 'this is coming from the server'
+    },
+    {
+      id:'wekngmakdwo',
+      title: 'second server side post',
+      content:'this is coming from the server as well'
+    }
+  ]
+  res.status(200).json({
+    message: 'posts fetched successfully',
+    posts: posts
+  });
 });
 
 
